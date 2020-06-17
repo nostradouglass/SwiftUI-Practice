9f306a53b7cbdf675405090ca0845310707aa4de
Kelly Douglass <kdouglass@gmail.com>
2020-06-16 23:35:38 -0500

    Initial Commit

---
+++ SwiftUI Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Practice
+//
+//  Created by Kelly Douglass on 6/16/20.
+//  Copyright © 2020 Kelly Douglass. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
